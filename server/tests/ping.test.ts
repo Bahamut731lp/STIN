b775f15f912b2ac199bd9d78b9f524d6ccb4fffb
Kevin Daněk <heindrich73@gmail.com>
2023-05-01 21:05:24 +0200

    feat: Přidán soubor s testy pro ping
    Tento soubor slouží jako šablona pro další testy

---
+++ server/tests/ping.test.ts
@@ -0,0 +1,15 @@
+import { superoak } from "https://deno.land/x/superoak@4.7.0/mod.ts";
+
+import app from "../server.ts"
+
+Deno.test("Ping #1: Response has status 200", async () => {
+    const request = await superoak(app);
+    await request.get("/ping").expect(200);
+});
+
+Deno.test("Ping #2: Response is in correct format", async () => {
+    const request = await superoak(app);
+    await request.get("/ping")
+    .expect("Content-Type", /json/)
+    .expect({data: "Pong!"});
+});

325377c890f128bd19e26937b5f207eb547e5abe
Kevin Daněk <heindrich73@gmail.com>
2023-05-05 17:51:58 +0200

    feat: Registrace

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { Route } from "wouter";
 import Homepage from "./pages/Homepage";
 import Login from "./pages/Login";
 import Logout from "./pages/Logout";
+import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
             <Route path="/" component={Homepage} />
             <Route path="/login" component={Login} />
             <Route path="/logout" component={Logout} />
+            <Route path="/register" component={Register} />
             <Route path="/dashboard" component={Dashboard} />
         </>
     )

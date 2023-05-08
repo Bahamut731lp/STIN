db8c56fac0ca6e56ef4b6d51fa8db738dd6efcc9
Kevin Daněk <heindrich73@gmail.com>
2023-05-08 23:02:33 +0200

    test: Coverage headerutilů

--- server/database/headers.ts
+++ server/database/headers.ts
@@ -5,8 +5,15 @@ class Headers {
         const headerSplitByWhitespace = input.split(/\s+/gi);
         if (headerSplitByWhitespace?.length < 2) return null;
 
-        const base64DecodedCredentials = atob(headerSplitByWhitespace.pop()!);
-        const splittedCredentials = base64DecodedCredentials.split(":");
+        let base64DecodedCredentials;
+        try {
+            base64DecodedCredentials = atob(headerSplitByWhitespace.pop()!);
+        }
+        catch(_) {
+            return null;
+        }
+
+        const splittedCredentials = base64DecodedCredentials.split(":").filter(v => v);
         if (splittedCredentials?.length < 2) return null;
         
         const [email, token] = splittedCredentials;

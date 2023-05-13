f552bc54e69d4172632d0148ad6782743d415bd4
Kevin Daněk <heindrich73@gmail.com>
2023-05-13 22:50:44 +0200

    Add CD server (#21)
    * Feature: CD Server (#20)
    
    * feat: Deployment webhook code
    
    * feat: Deployment webhook code
    
    * Feature cd server (#22)
    
    * feat: Deployment webhook code
    
    * feat: Deployment webhook code
    
    * Delete .env
    
    * fix: dostal jsem bídu od linteru

---
+++ .deployment/index.ts
@@ -0,0 +1,50 @@
+import { Application, Context, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
+import { load } from "https://deno.land/std/dotenv/mod.ts";
+
+const env = await load();
+if (!env["PASSWORD"] || !env["USERNAME"]) throw new Error();
+
+async function runBuild() {
+    const response = await new Deno.Command("docker-compose", { args: ["up", "-d", "--force-recreate"] }).output();
+    console.log(response)
+}
+
+async function handle(ctx: Context) {
+    if (!ctx.request.hasBody) {
+        ctx.response.status = 400;
+        return;
+    }
+
+    const body = await ctx.request.body().value;
+    try {
+        const json = JSON.parse(JSON.stringify(body));
+        const { username, password } = json;
+
+        if (!username || !password) throw new Error();
+        if (password == env["PASSWORD"] && username == env["USERNAME"]) {
+            runBuild();
+            ctx.response.status = 200;
+            return;
+        }
+        else {
+            ctx.response.status = 403;
+            return;
+        }
+
+    } catch (_) {
+        ctx.response.status = 400;
+        return;
+    }
+}
+
+const app = new Application();
+const router = new Router();
+
+router
+    .post("/", handle)
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+console.log("Running deployment webhook on 8001")
+await app.listen({ port: 8001 });

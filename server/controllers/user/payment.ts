ef5acb632a026e09d8ea31e2d67102cd899a88f2
Kevin Daněk <heindrich73@gmail.com>
2023-06-01 09:11:11 +0200

    fix: Kontokorent už kontokorentuje

--- server/controllers/user/payment.ts
+++ server/controllers/user/payment.ts
@@ -50,6 +50,8 @@ export async function post(context: Context) {
         if (amount > overdraft) continue;
         if (!conversion) continue;
 
+        if (amount > account.amount) paymentToBeMade -= (account.amount - paymentToBeMade) * 0.1;
+
         User.updateAccountWithPrefix(credentials.email, account.identifier.prefix, (document) => {
             document.amount -= Math.abs(paymentToBeMade);
             document.history.push({
